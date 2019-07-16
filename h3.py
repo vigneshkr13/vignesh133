6059556d83560b8c0efc307026ed68b40db965e2
vigneshkr13 <52488796+vigneshkr13@users.noreply.github.com>
2019-07-16 15:11:17 +0530

    Update h3.py

--- h3.py
+++ h3.py
@@ -1,10 +1,10 @@
-inp=int(input())                       
-val=list(map(int,input().split()))   
-final=[]
-for i in range(len(val)):
+aa=int(input())                       
+bb=list(map(int,input().split()))   
+l=[]
+for i in range(len(bb)):
     if i == val[i]:                
-        final.append(str(val[i]))
-if len(final)!=0:
-    print(" ".join(final))               
+        l.append(str(val[i]))
+if len(l)!=0:
+    print(" ".join(l))               
 else:
     print('-1')
