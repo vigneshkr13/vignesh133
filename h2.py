aa3c40105cfc40d044cd8ba6d43bbdd2e05c254d
vigneshkr13 <52488796+vigneshkr13@users.noreply.github.com>
2019-07-16 14:57:07 +0530

    Update h2.py

--- h2.py
+++ h2.py
@@ -1,8 +1,10 @@
 aa=int(input())
-if(aa>=1 and aa<=100000):
-  la=[]
-  for i in range(0,aa):
-    la.append(input())
-  la.sort(reverse=True)
-  for i in la:
-    print(i,end="")
+bb=input("")
+cc=list(bb.split(" "))
+cc.sort(reverse=True)
+bb=list(map(int,cc))
+if sum(bb)==0:
+  print("0")
+else:
+  dd="".join(cc)
+  print(dd)
